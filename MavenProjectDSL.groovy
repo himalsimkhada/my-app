b03de8cd3fc8b72f344e0bf95077dc39d4edf2df
Himal Simkhada <46737825+himalsimkhada@users.noreply.github.com>
2022-05-09 10:19:48 +0545

    Update MavenProjectDSL.groovy

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -2,7 +2,7 @@ job('example') {
     description("First Maven demo project")
     logRotator(-1, 1)
     scm {
-        github('https://github.com/himalsimkhada/my-app.git', 'master')
+        git('https://github.com/himalsimkhada/my-app.git', 'master')
     }
     triggers {
         scm('* * * * *')
